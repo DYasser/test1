c10c362e6a29c42c7e450fd90d65840da96fbdff
Yasser NABIL <yasser.nabil@cern.ch>
2022-06-24 14:17:56 +0200

    galery first

--- src/index.ts
+++ src/index.ts
@@ -3,80 +3,143 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { ICommandPalette, MainAreaWidget} from '@jupyterlab/apputils';
+
+//import {PageConfig} from "@jupyterlab/coreutils";
 
 import { Widget } from '@lumino/widgets';
 
-interface APODResponse {
-  copyright: string;
-  date: string;
-  explanation: string;
-  media_type: 'video' | 'image';
-  title: string;
-  url: string;
-};
+import {request} from "requests-helper";
 
-/**
- * Initialization data for the test1 extension.
- */
+import "../style/base.css";
+
+let path = "";
+let unique = 0;
+let local_file = false;
+
+class APODWidget extends Widget {
+  constructor() {
+    super();
+
+    path = "https://swan-gallery.web.cern.ch/";
+    this.init();
+  }
+
+  async init(){
+    const iconClass = `favicon-${unique}`;
+    this.title.iconClass = iconClass;
+    this.title.label = path;
+    this.title.closable = true;
+
+    //local_file = path.startsWith("local://");
+
+    unique += 1;
+
+    /*if (!local_file && !path.startsWith("http")) {
+      // use https, its 2020
+      path = `https://${path}`;
+    }*/
+
+    const div = document.createElement("div");
+    div.classList.add("iframe-widget");
+    const iframe = document.createElement("iframe");
+
+    if (!local_file) {
+      // External website
+
+      // First try to get directly
+      const res = await request("get", path);
+      if (res.ok && !res.headers.includes("Access-Control-Allow-Origin")) {
+        // Site does not disable iframe
+
+        // eslint-disable-next-line no-console
+        console.log("site accessible: proceeding");
+
+        iframe.src = path;
+
+        const favicon_url = new URL("/favicon.ico", path).href;
+
+        const res2 = await request("get", favicon_url);
+        if (res2.ok) {
+          const style = document.createElement("style");
+          style.innerHTML = `div.${iconClass} { background: url("${favicon_url}"); }`;
+          document.head.appendChild(style);
+        }
+      } /*else {
+        // Site is blocked for some reason, so attempt to proxy through python.
+        // Reasons include: disallowing iframes, http/https mismatch, etc
+
+        // eslint-disable-next-line no-console
+        console.log(`site failed with code ${res.status.toString()}`);
+
+        // eslint-disable-next-line no-empty
+        if (res.status === 404) {
+
+        } else {
+          // otherwise try to proxy
+          const favicon_url = `${PageConfig.getBaseUrl()}iframes/proxy?path=${new URL("/favicon.ico", path).href}`;
+
+          path = `iframes/proxy?path=${encodeURI(path)}`;
+          iframe.src = PageConfig.getBaseUrl() + path;
+          // eslint-disable-next-line no-console
+          console.log(`setting proxy for ${path}`);
+
+          const res2 = await request("get", favicon_url);
+          if (res2.ok) {
+            const style = document.createElement("style");
+            style.innerHTML = `div.${iconClass} { background: url("${favicon_url}"); }`;
+            document.head.appendChild(style);
+          }
+        }
+      }*/
+    } /*else {
+      // Local file, replace url and query for local
+      // eslint-disable-next-line no-console
+      console.log(`fetching local file ${path}`);
+      path = `iframes/local?path=${encodeURI(path.replace("local://", ""))}`;
+      iframe.src = PageConfig.getBaseUrl() + path;
+      }*/
+
+    div.appendChild(iframe);
+    this.node.appendChild(div);
+  }
+}
+
+function activate(app: JupyterFrontEnd, palette: ICommandPalette) {
+  console.log('JupyterLab extension Gallery is activated!');
+
+  // Add an application command
+  const command: string = 'apod:open';
+  app.commands.addCommand(command, {
+    label: 'Gallery',
+    execute: () => {      
+      // Create a single widget
+      const content = new APODWidget();
+      const widget = new MainAreaWidget({content});
+      widget.id = 'swan-gallery';
+      widget.title.label = 'SWAN Gallery';
+      widget.title.closable = true;
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.add(widget, 'main');
+      }
+      // Refresh the picture in the widget
+      content.update();
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({ command, category: 'Tutorial' });
+}
+
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'test1:plugin',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_test1 is activated!');
-      
+  activate: activate
     
-    // Add an application command
-    const command: string = 'test1:open';
-    
-    app.commands.addCommand(command, {
-      label: 'Random Astronomy Picture',
-      execute: async () => {
-        // Create a blank content widget inside of a MainAreaWidget
-        const content = new Widget();
-        const widget = new MainAreaWidget({ content });
-
-        // Add an image element to the content
-        let img = document.createElement('img');
-        content.node.appendChild(img);
-
-        // Get a random date string in YYYY-MM-DD format
-        function randomDate() {
-          const start = new Date(2010, 1, 1);
-          const end = new Date();
-          const randomDate = new Date(start.getTime() + Math.random()*(end.getTime() - start.getTime()));
-          return randomDate.toISOString().slice(0, 10);
-        }
-
-        // Fetch info about a random picture
-        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`);
-        const data = await response.json() as APODResponse;
-
-        if (data.media_type === 'image') {
-          // Populate the image
-          img.src = data.url;
-          img.title = data.title;
-        } else {
-          console.log('Random APOD was not a picture.');
-        }
-
-        widget.id = 'test1-jupyterlab';
-        widget.title.label = 'Astronomy Picture';
-        widget.title.closable = true;
-
-        if (!widget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          app.shell.add(widget, 'main');
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
-    });
-  
-    // Add the command to the palette.
-    palette.addItem({ command, category: 'Tutorial' });
-  }
 };
 
 export default plugin;
