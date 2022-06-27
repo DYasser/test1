677f4bfb9c36783041c1a7648ed35cfc7212d7d9
Yasser Nabil <ynabil14@gmail.com>
2022-06-27 16:38:33 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -5,76 +5,39 @@ import {
 
 import { ICommandPalette} from '@jupyterlab/apputils';
 
-import { Widget } from '@lumino/widgets';
-
-import {request} from "requests-helper";
-
-import "../style/base.css";
-
-let path = "";
-
-class APODWidget extends Widget {
-  constructor() {
-    super();
-
-    path = "https://swan-gallery.web.cern.ch/";
-    this.init();
-  }
-
-  async init(){
-    const div = document.createElement("div");
-    div.classList.add("iframe-widget");
-    const iframe = document.createElement("iframe");
-
-    // First try to get directly
-    const res = await request("get", path);
-    if (res.ok && !res.headers.includes("Access-Control-Allow-Origin")) {
-      // Site does not disable iframe
-
-      // eslint-disable-next-line no-console
-      console.log("site accessible: proceeding");
-
-      iframe.src = path;
-    }
-
-    div.appendChild(iframe);
-    this.node.appendChild(div);
-  }
-}
-
-function activate(app: JupyterFrontEnd, palette: ICommandPalette) {
+import { IFrame, MainAreaWidget} from '@jupyterlab/apputils';
+ 
+async function activate(
+  app: JupyterFrontEnd,
+  palette: ICommandPalette
+):Promise<void> {
   console.log('JupyterLab extension Gallery is activated!');
-
-  // Add an application command
   const command: string = 'gallery:open';
   app.commands.addCommand(command, {
-    label: 'Gallery',
-    execute: () => {      
-      // Create a single widget
-      let widget = new APODWidget();
+    label: `Gallery`,
+    execute: () => 
+    {
+      let content = new IFrame({
+        sandbox: ['allow-scripts', 'allow-forms', 'allow-same-origin']
+      });
+      content.url = "https://swan-gallery.web.cern.ch/";
+      content.title.label = "Gallery";
+      let widget = new MainAreaWidget({ content });
       widget.id = 'swan-gallery';
-      widget.title.label = 'SWAN Gallery';
       widget.title.closable = true;
-      if (!widget.isAttached) {
-        // Attach the widget to the main work area if it's not there
-        app.shell.add(widget, 'main');
-      }
-      // Activate the widget
+      app.shell.add(widget, 'main');
       app.shell.activateById(widget.id);
     }
   });
-
   // Add the command to the palette.
-  palette.addItem({ command, category: 'Sites' });
+  palette.addItem({ command, category: 'Tutorial' });
 }
 
-const plugin: JupyterFrontEndPlugin<void> = {
+const extension: JupyterFrontEndPlugin<void> = {
   id: 'test1:plugin',
-  autoStart: true,
   requires: [ICommandPalette],
-  activate
-    
+  autoStart: true,
+  activate: activate
 };
 
-export default plugin;
- 
+export default extension;
